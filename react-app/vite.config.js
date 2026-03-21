250641cf9e909d0d36fdcf0c1c259d3d8593149e
tamurayuuiti <314takamorisan@gmail.com>
2026-03-21 18:19:41 +0900

    refactor: React + Tailwind v4環境の初期構築完了

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // これを追加
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // これを追加
+  ],
 })
